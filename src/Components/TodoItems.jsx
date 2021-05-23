2a053939339425980d2cadab0231d078ad0bc4e2
branyers <32084718+branyers@users.noreply.github.com>
2021-05-23 19:41:41 -0400

    Update TodoItems.jsx

--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const TodoItems = ({taskList, deleteOnChange, UpdateCheckbox}) => {
     const {id,task,student,isCompleted} = taskList
    console.log(taskList)
-   console.log(_id)
     return (
         <div>
         <input type="checkbox" defaultChecked={isCompleted} onChange={(e)=>UpdateCheckbox(taskList,e)}/>
